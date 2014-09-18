9dbc85d451715e08f2fdb25aebea9609634fe83e
Leonardo Lee <lee.leonardo.king@gmail.com>
2014-09-17 22:14:12 -0700

    New repo start

---
+++ Enso/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Enso
+//
+//  Created by Leonardo Lee on 9/17/14.
+//  Copyright (c) 2014 Leonardo Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
