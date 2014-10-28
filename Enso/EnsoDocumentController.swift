562302d814591799bcb675e0d5bc181c3caffa63
Leonardo Lee <lee.leonardo.king@gmail.com>
2014-10-28 12:23:02 -0700

    Prior updates, not too sure exactly what I did.

---
+++ Enso/EnsoDocumentController.swift
@@ -0,0 +1,13 @@
+//
+//  EnsoDocumentController.swift
+//  Enso
+//
+//  Created by Leonardo Lee on 9/17/14.
+//  Copyright (c) 2014 Leonardo Lee. All rights reserved.
+//
+
+import UIKit
+
+class EnsoDocumentController {
+    
+}
