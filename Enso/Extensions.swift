24123f14de9380630f83916275c694c770ceb56c
Leonardo Lee <lee.leonardo.king@gmail.com>
2014-11-03 16:14:01 -0800

    This is a update, not much changing, working on the Switification of Liang's algorithm

---
+++ Enso/Extensions.swift
@@ -0,0 +1,25 @@
+//
+//  Extensions.swift
+//  Enso
+//
+//  Created by Leonardo Lee on 11/3/14.
+//  Copyright (c) 2014 Leonardo Lee. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    func countSyllables(fromString string: String) -> Int {
+        //string.lowercaseString
+        
+        return 0
+    }
+    
+    func breakDownSyllable() {
+        
+    }
+    
+    func liangAlgorithm() {
+        
+    }
+}
